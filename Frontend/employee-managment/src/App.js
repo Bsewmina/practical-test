e9a1b035340ea554d784fd2994a692bb804972b4
Binara <bsewmina@gmail.com>
2023-08-18 23:39:26 +0530

    home , nav page implemented

---
+++ Frontend/employee-managment/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Home from './components/home';
+import Header from './components/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
