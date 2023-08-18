4ea56f1268a8b4b1eda85a4dadd1e97f9496f41d
Binara <bsewmina@gmail.com>
2023-08-18 23:35:38 +0530

    home,nav page implemented

---
+++ Frontend/employee-managment/src/components/home.js
@@ -0,0 +1,54 @@
+import React, { useState , useEffect } from "react";
+import axios from "axios";
+import { Link } from 'react-router-dom';
+
+function Home (){
+
+    const [employees, setEmployees] = useState([]);
+
+
+    useEffect(() => {
+        getEmployes();
+    }, []);
+
+    const getEmployes = async () => {
+
+        const result = await axios.get("http://localhost:3006/api/v1/employee/");
+        console.log(result.data);
+        setEmployees(result.data);
+
+    };
+
+    return (
+        <div className="container">
+                <h3 className="p-3 text-center">List of Employees</h3>
+                <table className="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>EmployeeID</th>
+                            <th>Name</th>
+                            <th>EPF</th>
+                            <th>Section</th>
+                            <th>Gender</th>
+                            <th>DOB</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {employees && employees.map(employee => <tr key={employee.empId}>
+                            <td>{employee.name}</td>
+                            <td>{employee.epfNum}</td>
+                            <td>{employee.section}</td>
+                            <td>{employee.gender}</td>
+                            <td>{employee.dob}</td>
+                            <td><Link className='btn btn-outline-primary mx-2' to={`/edit/${employee.empId}`}>Edit</Link>
+                            </td>
+                        </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+    )
+    
+}
+
+export default Home;
